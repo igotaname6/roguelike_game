606f30c68e7c2a9db218c398d6f009ed810c5b44
Iwo <iwo.tyszkowski@gmail.com>
2017-05-18 21:38:57 +0200

    levels changed

--- stages.py
+++ stages.py
@@ -122,11 +122,10 @@ def player_move(key_input, x, y, board):
         x += 1
         if board[y][x] in obstacles:
             x -= 1
-    elif key_input == 'e':  # show inventory
-        broadcast = "change"
-        return x, y, broadcast
     if board[y][x] == '┼':
         broadcast = "doors"
+    if [x, y] == [94, 31]:
+        broadcast = 'next_lvl'
     return x, y, broadcast
 
 
@@ -178,44 +177,148 @@ def main():
     questions_list = [['2 + 2 = ', '4'],
                       ['2 + 2 * 2 = ', '6'],
                       ['arka gdynia? ', 'kurwa świnia']]
+    inventory = {'gówna': 1, 'patyki': 6}   # poczatkowy inwentarz
 
     '''First stage. '''
     os.system('clear')
-    player_interactions = [2, 2]  # starting position # list with factors depending on game progress
+    player_interactions = [2, 2]  # starting position
     board_change = "board1.csv"
-    inventory = {'gówna': 1, 'patyki': 6}
     board = create_board(board_change)
-
     while True:
         interactions_on_board = insert_player(board, player_interactions[0], player_interactions[1])
         print_board(interactions_on_board)
         print_inventory(inventory)
-
         print(player_interactions)     # testowo
-
         key_input = getch()
         player_interactions = player_move(key_input, player_interactions[0], player_interactions[1], board)
 
-        if player_interactions[2] is None:
-            board_change = "board1.csv"   # do napsania funkcja zmieniająca plansze.
-
-        elif player_interactions[2] is "change":
-            board_change = "menu.csv"
-
-        elif player_interactions[2] is 'doors':
+        if player_interactions[2] is 'doors':
             questions_no = random.choice(range(len(questions_list)))
             question = input(questions_list[questions_no][0])
             while question != questions_list[questions_no][1]:
                 question = input(questions_list[questions_no][0])
-            os.system('clear')
-
-        elif player_interactions[2] is 'most1':
-            pass
-
-        elif player_interactions[2] is 'budynek1':
-            pass
-
+        elif player_interactions[2] is 'next_lvl':
+            break
         os.system('clear')
+
+    '''Second stage. '''
+    os.system('clear')
+    player_interactions = [2, 2]  # starting position
+    board_change = "board2.csv"
+    inventory = {'gówna': 1, 'patyki': 6}
+    board = create_board(board_change)
+    while True:
+        interactions_on_board = insert_player(board, player_interactions[0], player_interactions[1])
+        print_board(interactions_on_board)
+        print_inventory(inventory)
+        print(player_interactions)     # testowo
+        key_input = getch()
+        player_interactions = player_move(key_input, player_interactions[0], player_interactions[1], board)
+
+        if player_interactions[2] is 'doors':
+            questions_no = random.choice(range(len(questions_list)))
+            question = input(questions_list[questions_no][0])
+            while question != questions_list[questions_no][1]:
+                question = input(questions_list[questions_no][0])
+        elif player_interactions[2] is 'next_lvl':
+            break
+        os.system('clear')
+
+    '''Third stage. '''
+    os.system('clear')
+    player_interactions = [2, 2]  # starting position
+    board_change = "board3.csv"
+    board = create_board(board_change)
+    while True:
+        interactions_on_board = insert_player(board, player_interactions[0], player_interactions[1])
+        print_board(interactions_on_board)
+        print_inventory(inventory)
+        print(player_interactions)     # testowo
+        key_input = getch()
+        player_interactions = player_move(key_input, player_interactions[0], player_interactions[1], board)
+
+        if player_interactions[2] is 'doors':
+            questions_no = random.choice(range(len(questions_list)))
+            question = input(questions_list[questions_no][0])
+            while question != questions_list[questions_no][1]:
+                question = input(questions_list[questions_no][0])
+        elif player_interactions[2] is 'next_lvl':
+            break
+        os.system('clear')
+
+    '''Fourth stage. '''
+    os.system('clear')
+    player_interactions = [2, 2]  # starting position
+    board_change = "board4.csv"
+    board = create_board(board_change)
+    while True:
+        interactions_on_board = insert_player(board, player_interactions[0], player_interactions[1])
+        print_board(interactions_on_board)
+        print_inventory(inventory)
+        print(player_interactions)     # testowo
+        key_input = getch()
+        player_interactions = player_move(key_input, player_interactions[0], player_interactions[1], board)
+
+        if player_interactions[2] is 'doors':
+            questions_no = random.choice(range(len(questions_list)))
+            question = input(questions_list[questions_no][0])
+            while question != questions_list[questions_no][1]:
+                question = input(questions_list[questions_no][0])
+        elif player_interactions[2] is 'next_lvl':
+            break
+        os.system('clear')
+
+    '''The Boss stage - hotncold game. '''
+    os.system('clear')
+    board_change = "boss.csv"
+    while True:
+        interactions_on_board = insert_player(board, player_interactions[0], player_interactions[1])
+        print_board(interactions_on_board)
+        key_input = getch()
+        os.system('clear')
+
+        print('I am thinking of a 3-digit number. Try to guess what it is.\n\
+              Here are some clues:\n\
+              When I say:    That means:\n\
+              Cold       No digit is correct.\n\
+              Warm       One digit is correct but in the wrong position.\n\
+              Hot        One digit is correct and in the right position.\n\
+              I have thought up a number. You have 10 guesses to get it.')
+        random_digit = str(random.choice(range(100, 1000)))
+        # print(random_digit)
+        tries = 1
+        digit_input = input("Guess digit: ")
+        while digit_input != random_digit:
+            print('Guess #', tries)
+            if tries == 1:
+                pass
+            else:
+                digit_input = input("Guess digit: ")
+            list_random = [random_digit[0], random_digit[1], random_digit[2]]
+            list_input = [digit_input[0], digit_input[1], digit_input[2]]
+            index_of_hot = []
+            count_hot = 0
+            count_warm = 0
+            count_cold = 0
+            for i in range(len(list_input)):
+                if list_input[i] == list_random[i]:
+                    count_hot += 1
+                    index_of_hot.append(i)
+            list_to_check_warm = [0, 1, 2]
+            for i in range(len(index_of_hot)):
+                list_to_check_warm.remove(index_of_hot[i])
+            for i in list_to_check_warm:
+                if list_input[i] in list_random:
+                    count_warm += 1
+                else:
+                    count_cold += 1
+            if count_cold == 3:
+                print("Cold")
+            else:
+                print("Hot " * count_hot, "Warm " * count_warm)
+            tries += 1
+        print('You got it!')
+        break
 
     # '''Win screen.'''
     # while True:
@@ -237,6 +340,7 @@ def main():
         print_board(create_board("highscore.csv"))
         key_input = getch()
         break
+
 
 if __name__ == '__main__':
     main()
